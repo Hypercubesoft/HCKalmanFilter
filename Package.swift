9a4c4c1d5226b5f589815881ed39e18d5e8c49b1
Tim Wise <tim@leisurehound.com>
2020-08-15 14:04:19 -0700

    Added a custom path to Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HCKalmanFilter",
-            dependencies: ["Surge"]),
+            dependencies: ["Surge"],
+            path: "HCKalmanFilter"),
     ]
 )
